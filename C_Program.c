053c68ca1147a9174dbacfd0645cf215b09689a8
Ubuntu <ubuntu@ip-172-31-31-245.ec2.internal>
2019-02-27 07:41:25 +0000

    I am pushing all my files to GitHub

---
+++ C_Program.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+
+printf("Hello I am C Program");
+
+return 0;
+
+}
+
---
+++ hellojava.java
@@ -0,0 +1,9 @@
+class Hellojava {
+public static void main (String args[]) {
+
+System.Println("I am your Java Progarm. Thank You");
+
+	}
+
+}
+
